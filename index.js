fed1fffd85c04e18517f65c40454802105ccd6a8
Kintesh <i.am@kinte.sh>
2015-01-28 19:11:24 +0000

    init commit

---
+++ index.js
@@ -0,0 +1,4 @@
+/**
+ * Created by kintesh on 28/01/15.
+ */
+module.exports = require('./lib/slides');
---
+++ test/test.js
@@ -0,0 +1,13 @@
+/**
+ * Created by kintesh on 28/01/15.
+ */
+var assert = require("assert");
+
+describe('Test', function () {
+
+    it("Rigorous tests", function () {
+        assert.equal(true, true);
+        assert.notEqual(true, false);
+    });
+
+});
