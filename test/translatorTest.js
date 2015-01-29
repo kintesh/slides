e94be5e99021eef59298baba8335a49dab0f49e5
Kintesh <i.am@kinte.sh>
2015-01-29 20:19:48 +0000

    added tests for readProperties() method

--- test/translatorTest.js
+++ test/translatorTest.js
@@ -2,7 +2,10 @@
  * Created by kintesh on 28/01/15.
  */
 var assert = require("assert"),
-    translator = require("../lib/translator");
+    translator = require("../lib/translator"),
+    fs = require("fs"),
+    testSource
+    ;
 
 describe("Translator", function () {
 
@@ -10,6 +13,7 @@ describe("Translator", function () {
         assert.equal(true, true);
         assert.notEqual(true, false);
         assert.notDeepEqual(translator, {});
+        assert.notDeepEqual(fs, {});
     });
 
     it("Test removeComments(source)", function () {
@@ -17,6 +21,22 @@ describe("Translator", function () {
                 "Solar System's Oort cloud//.",
             output = "Tyche is the name given to a  gas giant located in the Solar System's Oort cloud";
         assert.equal(translator.removeComments(input), output);
+    });
+
+    it("read test source file", function() {
+        testSource = fs.readFileSync(__dirname+"/testSource", "utf8");
+        assert.notDeepEqual(testSource, undefined);
+    });
+
+    it("test for readProperties", function () {
+        var input = testSource,
+            output = {
+                title: 'Sample Slides',
+                sub_title: 'The following are sample slides showing different features.',
+                author: 'Kintesh Patel',
+                date: '28/01/2015'
+            };
+        assert.deepEqual(translator.readProperties(input), output);
     });
 
 });
