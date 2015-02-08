d0a4e8f1c535c6f912fe026f1c54f587f3da133b
Kintesh <i.am@kinte.sh>
2015-02-08 21:28:01 +0000

    updated.

--- test/test.js
+++ test/test.js
@@ -13,7 +13,10 @@ describe("Slides test", function () {
 
     it("test for translate", function() {
         var input = "====\n#Welcome to slides\n**Slides** is a tool to *make* and *present* HTML5 slides.====",
-            output = "<div class=\"slide\">\n<h1 id=\"welcome-to-slides\">Welcome to slides</h1>\n<p><strong>Slides</strong> is a tool to <em>make</em> and <em>present</em> HTML5 slides.</p>\n\n</div>\n";
+            output = "<!DOCTYPE html>\n<html>\n<head>\n<title>Slides</title>\n<meta charset=\"UTF-8\">\n</head>" +
+                "\n\n<body>\n<div class=\"slide\">\n<h1 id=\"welcome-to-slides\">Welcome to slides</h1>\n<p>" +
+                "<strong>Slides</strong> is a tool to <em>make</em> and <em>present</em> HTML5 slides.</p>\n\n</div>" +
+                "\n</body>\n</html>";
         assert.deepEqual(slides(input), output);
     });
 
