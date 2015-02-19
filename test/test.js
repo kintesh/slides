e24807fa851e9649fd3e96eb7e8965610bb20135
Kintesh <i.am@kinte.sh>
2015-02-19 12:36:45 +0000

    added styles and scripts

--- test/test.js
+++ test/test.js
@@ -13,7 +13,13 @@ describe("Slides test", function() {
 
     it("test for translate", function() {
         var input = "====\n# -> Welcome to slides <-\n**Slides** is a tool to *make* and *present* HTML5 slides.====",
-            output = "<!DOCTYPE html>\n<html>\n<head>\n<title>Slides</title>\n<meta charset=\"UTF-8\">\n</head>" +
+            output = "<!DOCTYPE html>\n<html>\n<head>\n<title>Slides</title>\n<meta charset=\"UTF-8\">\n" +
+                "<link rel=\"stylesheet\" href=\"./slides_assets/css/slides.css\">\n" +
+                "<link rel=\"stylesheet\" href=\"./slides_assets/css/slides-print.css\" media=\"print\" />" +
+                "<link rel=\"stylesheet\" href=\"./slides_assets/css/katex.css\">\n" +
+                "<link rel=\"stylesheet\" href=\"./slides_assets/css/font-awesome.css\">\n" +
+                "<script src=\"./slides_assets/js/jquery.js\"></script>\n" +
+                "<script src=\"./slides_assets/js/slides.js\"></script></head>" +
                 "\n\n<body>\n<div class=\"slide\">\n<h1 id=\"welcome-to-slides\" style=\"text-align: center;\">" +
                 "Welcome to slides</h1>\n<p><strong>Slides</strong> is a tool to <em>make</em> and <em>present</em> " +
                 "HTML5 slides.</p>\n\n</div>\n</body>\n</html>";
