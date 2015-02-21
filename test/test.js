ddeafda3f07b6d7365586fc3258660ca7fbea208
Kintesh <i.am@kinte.sh>
2015-02-21 18:06:01 +0000

    async, async everywhere.

--- test/test.js
+++ test/test.js
@@ -4,14 +4,14 @@
 var assert = require("assert"),
     slides = require("../lib/slides");
 
-describe("Slides test", function() {
+describe("Slides", function() {
 
     it("Rigorous tests", function() {
         assert.equal(true, true);
         assert.notEqual(true, false);
     });
 
-    it("test for translate", function() {
+    it("test for translate", function(done) {
         var input = "====\n# -> Welcome to slides <-\n**Slides** is a tool to *make* and *present* HTML5 slides.====",
             output = "<!DOCTYPE html>\n<html>\n<head>\n<title>Slides</title>\n<meta charset=\"UTF-8\">\n" +
                 "<link rel=\"stylesheet\" href=\"./slides_assets/css/slides.css\">\n" +
@@ -23,7 +23,11 @@ describe("Slides test", function() {
                 "\n\n<body>\n<div class=\"slide\">\n<h1 id=\"welcome-to-slides\" style=\"text-align: center;\">" +
                 "Welcome to slides</h1>\n<p><strong>Slides</strong> is a tool to <em>make</em> and <em>present</em> " +
                 "HTML5 slides.</p>\n\n</div>\n</body>\n</html>";
-        assert.deepEqual(slides(input), output);
+
+        slides(input, function(err, res) {
+            assert.deepEqual(res.html, output);
+            done();
+        });
     });
 
 });
--- test/translatorTest.js
+++ test/translatorTest.js
@@ -4,137 +4,242 @@
 var assert = require("assert"),
     translator = require("../lib/translator"),
     fs = require("fs"),
+    async = require("async"),
     testSource
     ;
 
-describe("Translator test", function() {
+describe("Translator", function() {
 
-    it("Rigorous tests", function() {
+    it.skip("Rigorous tests", function() {
         assert.equal(true, true);
         assert.notEqual(true, false);
         assert.notDeepEqual(translator, {});
         assert.notDeepEqual(fs, {});
     });
 
-    it("Test removeComments(source)", function() {
+    it("Test removeComments(source)", function(done) {
         var input = "Tyche is the /*nick*/name given to a /**hypothetical**/ gas giant located in the " +
                 "Solar System's Oort cloud//.",
-            output = "Tyche is the name given to a  gas giant located in the Solar System's Oort cloud";
-        assert.equal(translator.removeComments(input), output);
+            expOut = "Tyche is the name given to a  gas giant located in the Solar System's Oort cloud";
+        async.waterfall([
+            function(callback) {
+                translator.removeComments({source:input}, callback)
+            }
+        ], function(err, res) {
+            if(err == null) {
+                assert.deepEqual(res.source, expOut);
+                done();
+            }
+        });
     });
 
-    it("read test for source file", function() {
-        testSource = fs.readFileSync(__dirname+"/testSource", "utf8");
-        assert.notDeepEqual(testSource, undefined);
+    it("read test for source file", function(done) {
+        async.waterfall([
+            function(callback) {
+                fs.readFile(__dirname+"/testSource", "utf8", callback);
+            }
+        ], function(err, data) {
+            if(err == null) {
+                testSource = data;
+                done();
+            }
+        });
     });
 
-    it("test for readProperties", function() {
+    it("test for readProperties", function(done) {
         var input = testSource,
-            output = {
+            expOut = {
                 title: "Sample Slides",
                 sub_title: "Sample slides showing different features.",
                 author: "Kintesh Patel",
                 date: "28/01/2015"
             };
-        assert.deepEqual(translator.readProperties(input), output);
+        async.waterfall([
+            function(callback) {
+                translator.readProperties({source:input}, callback);
+            }
+        ], function(err, res) {
+            if(err == null) {
+                assert.deepEqual(res.properties, expOut);
+                done();
+            }
+        });
     });
 
-    it("test for extractFrames", function() {
+    it("test for extractFrames", function(done) {
         var input = testSource,
             frameLength = 7,
             frame0 = "[[background-color:aquamarine; color:black]]\n# Headers\n---------\n[[visibility:reveal]]" +
                 "\n# H1 header\n[[visibility:reveal; color:red]]\n## H2 header\n[[visibility:reveal; color:#22FF22]]" +
                 "\n### H3 header\n[[visibility:reveal]]\n#### H4 header\n[[visibility:reveal]]\n##### H5 header" +
                 "\n[[visibility:reveal]]\n###### H6 header\n\n";
-        var frames = translator.extractFrames(input);
-        assert.equal(frameLength, frames.length);
-        assert.deepEqual(frame0, frames[0]);
+        async.waterfall([
+            function(callback) {
+                translator.extractFrames({source:input}, callback);
+            }
+        ], function(err, res) {
+            if(err == null) {
+                assert.equal(frameLength, res.frames.length);
+                assert.deepEqual(frame0, res.frames[0].content);
+                done();
+            }
+        });
     });
 
-    it("test for extractMaths", function() {
+    it("test for extractMaths", function(done) {
         var inputFrame = "Inline math equations go in like so: $ \\omega = d\\phi / dt $. Display math should " +
                 "get its own line and be put in in double-dollarsigns:\n\n$$ I = \\int \\rho R^{2} dV $$\n\nAnd " +
                 "note that you can backslash-escape any punctuation characters.",
-            outputFrame = "Inline math equations go in like so: %{MATH0}. Display math should get its own " +
+            expOutFrame = "Inline math equations go in like so: %{MATH0}. Display math should get its own " +
                 "line and be put in in double-dollarsigns:\n\n%{MATH1}\n\nAnd note that you can backslash-escape " +
                 "any punctuation characters.",
             mathsOutput = [
                 { type: 0, value: " \\omega = d\\phi / dt " },
                 { type: 1, value: " I = \\int \\rho R^{2} dV " }
             ];
-        var output = translator.extractMaths(inputFrame);
-        assert.deepEqual(outputFrame, output.frame);
-        assert.deepEqual(mathsOutput, output.maths);
+        async.waterfall([
+            function(callback) {
+                translator.extractMaths({frames:[{content:inputFrame}]}, callback);
+            }
+        ], function(err, res) {
+            if(err == null) {
+                assert.deepEqual(expOutFrame, res.frames[0].content);
+                assert.deepEqual(mathsOutput, res.frames[0].maths);
+                done();
+            }
+        });
     });
 
-    it("test for restoreMaths", function() {
+    it("test for restoreMaths", function(done) {
         var inputFrame = "Inline math equations go in like so: %{MATH0}. Display math should get its own " +
                 "line and be put in in double-dollarsigns:\n\n%{MATH1}\n\nAnd note that you can backslash-escape " +
                 "any punctuation characters.",
-            outputFrame = "Inline math equations go in like so: <div class=\"inlineMath\"> \\omega = d\\phi / dt " +
-                "</div>. Display math should get its own line and be put in in double-dollarsigns:\n\n" +
-                "<div class=\"blockMath\"> I = \\int \\rho R^{2} dV </div>\n\nAnd note that you can " +
+            expOutFrame = "Inline math equations go in like so: <span class=\"inlineMath\"> \\omega = d\\phi / dt " +
+                "</span>. Display math should get its own line and be put in in double-dollarsigns:\n\n" +
+                "<p class=\"blockMath\"> I = \\int \\rho R^{2} dV </p>\n\nAnd note that you can " +
                 "backslash-escape any punctuation characters.",
             maths = [
                 { type: 0, value: " \\omega = d\\phi / dt " },
                 { type: 1, value: " I = \\int \\rho R^{2} dV " }
-            ],
-            replaceInline = "<div class=\"inlineMath\">%{MATH}</div>",
-            replaceBlock = "<div class=\"blockMath\">%{MATH}</div>";
-        assert.deepEqual(translator.restoreMaths(inputFrame, maths, replaceInline, replaceBlock), outputFrame);
+            ];
+        async.waterfall([
+            function(callback) {
+                translator.restoreMaths({frames:[{content:inputFrame, maths:maths}]}, callback)
+            }
+        ], function(err, res) {
+            if(err == null) {
+                assert.deepEqual(res.frames[0].content, expOutFrame);
+                done();
+            }
+        });
     });
 
-    it("test for replaceControlTags", function() {
+    it("test for replaceControlTags", function(done) {
         var inputFrame = "Itemized lists look like:\n[[visibility:reveal; color:red]]\n  * this one\n  " +
             "* that one\n  * the other one\n",
-            outputFrame = "Itemized lists look like:\n<div class=\"controlElem\" style=\"display: none;\">" +
+            expOutFrame = "Itemized lists look like:\n<div class=\"controlElem\" style=\"display: none;\">" +
                 "{\"visibility\":\"reveal\",\"color\":\"red\"}</div>\n  * this one\n  * that one\n  * the other one\n";
-        assert.deepEqual(translator.replaceControlTags(inputFrame), outputFrame);
+        async.waterfall([
+            function(callback) {
+                translator.replaceControlTags({frames:[{content:inputFrame}]}, callback)
+            }
+        ], function(err, res) {
+            if(err == null) {
+                assert.deepEqual(res.frames[0].content, expOutFrame);
+                done();
+            }
+        });
     });
 
-    it("test for makeTitleSlide", function(){
+    it("test for makeTitleSlide", function(done){
         var input = {
                 title: "Sample Slides",
                 sub_title: "Sample slides showing different features.",
                 author: "Kintesh Patel",
                 date: "28/01/2015"
             },
-            output = "<div class=\"slide\">\n<div class=\"title_title\"><h1>Sample Slides</h1></div>" +
+            expOut = "<div class=\"slide\">\n<div class=\"title_title\"><h1>Sample Slides</h1></div>" +
                 "<div class=\"title_subTitle\"><h3>Sample slides showing different features.</h3></div>" +
                 "<div class=\"title_author\">Kintesh Patel</div><div class=\"title_date\">28/01/2015</div>\n</div>\n";
-        assert.deepEqual(translator.makeTitleSlide(input), output)
+        async.waterfall([
+            function(callback) {
+                translator.makeTitleSlide({properties:input}, callback);
+            }
+        ], function(err, res) {
+            if(err == null) {
+                assert.deepEqual(res.titleSlide, expOut);
+                done();
+            }
+        });
     });
 
-    it("test for readFrameProperties", function() {
-        var frames = translator.extractFrames(testSource),
+    it("test for readFrameProperties", function(done) {
+        var
             frame0 = "# Headers\n---------\n[[visibility:reveal]]\n# H1 header\n[[visibility:reveal; color:red]]" +
                 "\n## H2 header\n[[visibility:reveal; color:#22FF22]]\n### H3 header\n[[visibility:reveal]]" +
                 "\n#### H4 header\n[[visibility:reveal]]\n##### H5 header\n[[visibility:reveal]]" +
-                "\n###### H6 header\n\n",
-            output = translator.readFrameProperties(frames[0]);
-        assert.deepEqual(output.frame, frame0);
-        assert.deepEqual(output.properties, "background-color:aquamarine; color:black");
+                "\n###### H6 header\n\n";
+        async.waterfall([
+            function(callback) {
+                translator.extractFrames({source:testSource}, callback)
+            },
+            function(slides, callback) {
+                translator.readFrameProperties(slides, callback)
+            }
+        ], function(err, res) {
+            if (err == null) {
+                assert.deepEqual(res.frames[0].content, frame0);
+                assert.deepEqual(res.frames[0].properties, "background-color:aquamarine; color:black");
+                done();
+            }
+        });
     });
 
-    it("test for renderMarkdown", function() {
-        var input = "I am using **markdown**.",
-            output = "<p>I am using <strong>markdown</strong>.</p>\n";
-        assert.deepEqual(translator.renderMarkdown(input), output);
+    it("test for renderMarkdown", function(done) {
+        var inputFrame = "I am using **markdown**.",
+            expOut = "<p>I am using <strong>markdown</strong>.</p>\n";
+        async.waterfall([
+            function(callback) {
+                translator.renderMarkdown({frames:[{content:inputFrame}]}, callback);
+            }
+        ], function(err, res) {
+            if(err == null) {
+                assert.deepEqual(res.frames[0].content, expOut);
+                done();
+            }
+        });
     });
 
-    it("test for renderKatex", function() {
+    it("test for renderKatex", function(done) {
         var input = "$ \\pi $",
-            output = translator.extractMaths(input),
-            rendered = "<span class=\"katex\"><span class=\"katex-inner\"><span class=\"strut\" " +
+            expOut = "<span class=\"katex\"><span class=\"katex-inner\"><span class=\"strut\" " +
                 "style=\"height:0.43056em;\"></span><span class=\"strut bottom\" style=\"height:0.43056em;" +
                 "vertical-align:0em;\"></span><span class=\"base textstyle uncramped\"><span class=\"mord " +
                 "mathit\" style=\"margin-right:0.03588em;\">π</span></span></span></span>";
-        assert.deepEqual(translator.renderKatex(output.frame, output.maths),rendered)
-
+        async.waterfall([
+            function(callback) {
+                translator.extractMaths({frames:[{content:input}]}, callback);
+            },
+            function(slides, callback) {
+                translator.renderKatex(slides, callback);
+            }
+        ], function(err, res) {
+            if(err == null) {
+                assert.deepEqual(res.frames[0].content, expOut);
+                done();
+            }
+        });
     });
 
-    it.skip("test for translate", function() {
-        console.log(translator.translate(testSource));
+    it("test for translate", function(done) {
+        async.waterfall([
+            function(callback) {
+                translator(testSource, callback);
+            }
+        ], function(err, res) {
+            assert.notDeepEqual(res.html,undefined);
+            done();
+        })
     });
 
 });
