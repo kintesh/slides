65ac90620648f928081be0ac64a2e58a790618cf
Kintesh <i.am@kinte.sh>
2015-02-26 16:40:57 +0000

    added event listener for angle brackets.

--- assets/js/slides.js
+++ assets/js/slides.js
@@ -361,9 +361,9 @@ var Slides = (function($) {
     $( document ).keypress(function( event ) {
         //console.log("keyCode: " + event.keyCode + " | which:" + event.which + " | key:" + event.key);
         // Left:37  Up:38  Right:39  Down:40  13:Return
-        if(event.keyCode == 39 || event.keyCode == 40) {
+        if(event.keyCode == 39 || event.keyCode == 40 || event.keyCode == 46) {
             next();
-        } else if (event.keyCode == 37 || event.keyCode == 38) {
+        } else if (event.keyCode == 37 || event.keyCode == 38 || event.keyCode == 44) {
             previous();
         } else if (event.keyCode == 13) {
             toggleFullScreen();
