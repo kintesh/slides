2cd6f852a891d990ca121b247ec3e68b7db71491
Kintesh <i.am@kinte.sh>
2015-03-10 13:16:22 +0000

    remove // comments functions.

--- test/translatorTest.js
+++ test/translatorTest.js
@@ -19,8 +19,8 @@ describe("Translator", function() {
 
     it("Test removeComments(source)", function(done) {
         var input = "Tyche is the /*nick*/name given to a /**hypothetical**/ gas giant located in the " +
-                "Solar System's Oort cloud//.",
-            expOut = "Tyche is the name given to a  gas giant located in the Solar System's Oort cloud";
+                "Solar System's Oort cloud.",
+            expOut = "Tyche is the name given to a  gas giant located in the Solar System's Oort cloud.";
         async.waterfall([
             function(callback) {
                 translator.removeComments({source:input}, callback)
