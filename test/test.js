62c3da5bcc183456c7123fe85cb3733e4cd37794
Kintesh <i.am@kinte.sh>
2015-04-07 22:37:12 +0100

    all slides assets are on kinte.sh

--- test/test.js
+++ test/test.js
@@ -31,15 +31,15 @@ describe("Slides", function() {
                 "</h1>\n<p><strong>Slides</strong> is a tool to <em>make</em> and <em>present</em> HTML5 slides.</p>" +
                 "\n\n</div>\n</body>\n</html>",
             online = "<!DOCTYPE html>\n<html>\n<head>\n<title>Slides</title>\n<meta charset=\"UTF-8\">\n" +
-                "<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700|Source+Code+Pro\">\n" +
+                "<link rel=\"stylesheet\" href=\"https://slides.kinte.sh/slides_assets/css/fonts.css\">\n" +
                 "<link rel=\"stylesheet\" href=\"https://slides.kinte.sh/slides_assets/css/slides.css\">\n" +
                 "<link rel=\"stylesheet\" href=\"https://slides.kinte.sh/slides_assets/css/slides-print.css\" media=\"print\"/>\n" +
                 "<link rel=\"stylesheet\" href=\"https://slides.kinte.sh/slides_assets/css/style.css\"/>\n" +
                 "<link rel=\"stylesheet\" href=\"https://slides.kinte.sh/slides_assets/css/animations.css\"/>\n" +
-                "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css\">\n" +
-                "<script src=\"https://code.jquery.com/jquery-2.1.3.min.js\"></script>\n" +
+                "<link rel=\"stylesheet\" href=\"https://slides.kinte.sh/slides_assets/css/font-awesome.css\">\n" +
+                "<script src=\"https://slides.kinte.sh/slides_assets/js/jquery.js\"></script>\n" +
                 "<script src=\"https://slides.kinte.sh/slides_assets/js/slides.js\"></script>\n" +
-                "<script src=\"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML\"></script>\n" +
+                "<script src=\"https://slides.kinte.sh/slides_assets/js/MathJax/MathJax.js?config=TeX-AMS-MML_HTMLorMML\"></script>\n" +
                 "</head>\n\n<body>\n" +
                 "<div class=\"slide\">\n<h1 id=\"welcome-to-slides\" style=\"text-align: center;\">Welcome to slides" +
                 "</h1>\n<p><strong>Slides</strong> is a tool to <em>make</em> and <em>present</em> HTML5 slides.</p>" +
